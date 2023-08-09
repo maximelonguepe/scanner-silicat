5361bdf26b0bc23ace5403a61e40ac55a70f5c5a
maximelonguepe <longuepemaxime73@gmail.com>
2023-08-10 00:57:38 +0200

    Adding download on barcode

--- src/DetailObjet.tsx
+++ src/DetailObjet.tsx
@@ -1,8 +1,9 @@
 import {useLocation} from "react-router-dom";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Consommable, Objet, Profil} from "./types";
 import {Button, Col, FormControl, FormGroup, Row} from "react-bootstrap";
 import Barcode from "react-barcode";
+import html2canvas from "html2canvas";
 
 const DetailObjet = () => {
     const [loading, setLoading] = useState(true);
@@ -10,6 +11,7 @@ const DetailObjet = () => {
     const [error, setError] = useState<string | null>(null);
     const [consommable, setConsommable] = useState<Consommable | null>(null);
     const [profil, setProfil] = useState<Profil | null>(null)
+    const qrCodeRef = useRef(null);
 
     const [editingPUConsommable, setEditingPUConsommable] = useState(false);
     const [editingReferenceConsommable, setEditingReferenceConsommable] = useState(false);
@@ -19,7 +21,6 @@ const DetailObjet = () => {
         try {
             const response = await fetch(`http://localhost:8080/api/objets/get?id=${id}`);
             const jsonData = await response.json();
-            console.log(jsonData);
             setObjet(jsonData);
             setLoading(false);
         } catch (error) {
@@ -33,7 +34,6 @@ const DetailObjet = () => {
         try {
             const response = await fetch(`http://localhost:8080/api/consommables/get?id=${id}`);
             const jsonData = await response.json();
-            console.log(jsonData);
             setConsommable(jsonData);
             setLoading(false);
         } catch (error) {
@@ -56,6 +56,40 @@ const DetailObjet = () => {
             setLoading(false);
         }
     };
+    const handleDownloadClick = () => {
+        const barcodeDiv = document.getElementById('barcode');
+        if (barcodeDiv) {
+            html2canvas(barcodeDiv).then((canvas) => {
+                const link = document.createElement('a');
+                link.download = 'barcode'+objet?.id+'.png';
+                link.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+                link.click();
+            });
+        }
+    };
+    const updateConsommable = async () => {
+        try {
+            // Mettez à jour les propriétés nécessaires de l'objet
+
+            const response = await fetch('http://localhost:8080/api/consommables', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(consommable),
+            });
+
+            if (response.ok) {
+                console.log('Objet mis à jour avec succès.');
+                // Vous pouvez appeler fetchData() pour récupérer les données mises à jour après le PUT
+            } else {
+                console.error('Erreur lors de la mise à jour de l\'objet.');
+            }
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour:', error);
+        }
+    };
+
 
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
@@ -78,10 +112,8 @@ const DetailObjet = () => {
         if (objet) {
 
             if (objet.type === "CONSOMMABLE") {
-                console.log('consommable');
                 fetchConsommable(objet.id.toString());
             } else if (objet.type === "PROFIL") {
-                console.log('profil');
                 fetchProfil(objet.id.toString());
             }
         }
@@ -110,6 +142,13 @@ const DetailObjet = () => {
     const handleEditClickConsommableQuantite = () => {
         setEditingQuantiteConsommable(true);
     };
+
+    function handleSaveModif() {
+        if (objet?.type === 'CONSOMMABLE') {
+            updateConsommable();
+        }
+    }
+
     return (
         <>
             <br/>
@@ -188,9 +227,24 @@ const DetailObjet = () => {
                             Quantité :
                         </Col>
                         <Col>
-                            <span>
-                                {consommable.quantiteOuMl}
-                            </span>
+                            {
+                                editingQuantiteConsommable ?
+                                    (<>
+                                        <FormControl
+                                            type="text"
+                                            name="quantiteOuMl"
+                                            value={consommable.quantiteOuMl}
+                                            onChange={handleInputChange}
+                                            onBlur={handleInputConsommableQuantiteBlur}
+                                            autoFocus
+                                        />
+                                    </>) : <span> {consommable.quantiteOuMl}</span>
+                            }
+                        </Col>
+                        <Col>
+                            <Button onClick={handleEditClickConsommableQuantite} variant="primary">
+                                Modifier
+                            </Button>
                         </Col>
                     </Row>
                     <Row>
@@ -208,15 +262,24 @@ const DetailObjet = () => {
                     </Row>
                     <Row>
                         <Col>
-                            Qr code :
+                            Code barre:
 
                         </Col>
                         <Col>
+                        <div id='barcode'>
                             <Barcode value={consommable.id.toString()} format="CODE39"/>
+
+                        </div>
+
+                        </Col>
+                        <Col>
+                            <Button onClick={handleDownloadClick}>
+                                Télécharger
+                            </Button>
                         </Col>
                     </Row>
                     <Row>
-                        <Button>Sauvegarder Modifications</Button>
+                        <Button onClick={handleSaveModif}>Sauvegarder Modifications</Button>
                     </Row>
 
                 </>
@@ -237,9 +300,6 @@ const DetailObjet = () => {
                         <Col>
                             <Barcode value={profil.id.toString()} format="CODE39"/>
                         </Col>
-                    </Row>
-                    <Row>
-                        <Button>Sauvegarder Modifications</Button>
                     </Row>
 
                 </>
