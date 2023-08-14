8e93d6e563a10548ccd48ce372a7991b76551b95
maximelonguepe <longuepemaxime73@gmail.com>
2023-08-14 23:37:21 +0200

    Adding Modal when a profil is created

--- src/GenererObjet.tsx
+++ src/GenererObjet.tsx
@@ -1,15 +1,28 @@
-import {Row, Col, Form, Button, FormControl, Table} from "react-bootstrap";
+import {Row, Col, Form, Button, FormControl, Table, Modal} from "react-bootstrap";
 import React, {ChangeEvent, useState} from "react";
 import {apiUrl, Couleur, Profil} from "./types";
 import Barcode from "react-barcode";
 import {Link} from "react-router-dom";
+import html2canvas from "html2canvas";
 
 const GenererObjet = () => {
     const [typeSelected, setTypeSelected] = useState('PROFIL');
     const [isCreatingCouleur, setIsCreatingCouleur] = useState(false);
-    const [couleurToBeEdited, setCouleurToBeEdited]=useState<Couleur>({
+    const [couleurToBeEdited, setCouleurToBeEdited] = useState<Couleur>({
         id: null, metreLineaire: 0, nomCouleur: ""
 
+    })
+    const [showModalAjoutOk, setShowModalAjoutOk] = useState(false);
+
+    const [profilSaved, setProfilSaved] = useState<Profil>({
+        couleurs: [],
+        longueur: 0,
+        prixMetreLineaire: 0,
+        prixUnitaire: 0,
+        quantiteOuMl: 0,
+        referenceProduit: "",
+        type: "PROFIL",
+        id: 0
     })
     const [profil, setProfil] = useState<Profil>({
         couleurs: [],
@@ -22,9 +35,36 @@ const GenererObjet = () => {
         id: 0
     });
 
+    function handleCloseModalAjoutOk() {
+        setShowModalAjoutOk(false);
+        setProfil({
+            couleurs: [],
+            longueur: 0,
+            prixMetreLineaire: 0,
+            prixUnitaire: 0,
+            quantiteOuMl: 0,
+            referenceProduit: "",
+            type: "PROFIL",
+            id: 0
+        })
+    }
+
+
+    const handleDownloadClick = () => {
+        const barcodeDiv = document.getElementById('barcode');
+        if (barcodeDiv) {
+            html2canvas(barcodeDiv).then((canvas) => {
+                const link = document.createElement('a');
+                link.download = 'barcode' + profilSaved?.id + '.png';
+                link.href = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+                link.click();
+            });
+        }
+    };
+
     const saveProfilFetch = async () => {
         try {
-            const response = await fetch(apiUrl+"profils", {
+            const response = await fetch(apiUrl + "profils", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -35,6 +75,8 @@ const GenererObjet = () => {
             const data = await response.json();
 
             if (response.ok) {
+                setProfilSaved(data);
+                setShowModalAjoutOk(true);
 
             } else {
 
@@ -74,11 +116,12 @@ const GenererObjet = () => {
             return prevProfil; // Renvoie null ou le profil inchangé en fonction de votre logique
         });
     };
+
     function editCouleurs() {
         setIsCreatingCouleur(true);
     }
 
-    function hideEditingColor(){
+    function hideEditingColor() {
         setIsCreatingCouleur(false);
         setCouleurToBeEdited({
             id: null,
@@ -86,6 +129,7 @@ const GenererObjet = () => {
             metreLineaire: 0,
         })
     }
+
     const handleInputChangeNewColor = (event: React.ChangeEvent<HTMLInputElement>) => {
         const {name, value} = event.target;
         setCouleurToBeEdited((prevObject) => ({
@@ -244,6 +288,104 @@ const GenererObjet = () => {
                     </Row>
                 </>
             )}
+            <Modal show={showModalAjoutOk} onHide={handleCloseModalAjoutOk}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Ajout réussi</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col>
+                            Ajout réussi
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            Code barre
+                        </Col>
+                        <Col>
+                            <div id='barcode' style={{display: 'inline-block', padding: '0', margin: '0'}}>
+                                <Barcode value={profilSaved.id.toString()} format="CODE39"/>
+                            </div>
+                        </Col>
+                        <Col>
+                            <Button onClick={handleDownloadClick}>
+                                Télécharger
+                            </Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            Référence :
+
+                        </Col>
+                        <Col>
+                            {profilSaved.referenceProduit}
+
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            Prix au mètre linéaire :
+                        </Col>
+                        <Col>
+                            {profilSaved.prixUnitaire} €
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            Nombre de mètres linéaires
+                        </Col>
+                        <Col>
+                            {profilSaved.quantiteOuMl} m
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col>
+                            Prix total
+                        </Col>
+                        <Col>
+                            {profilSaved.quantiteOuMl*profilSaved.prixUnitaire} €
+                        </Col>
+                    </Row>
+                    <br/>
+                    {profilSaved.couleurs.length > 0 && (
+                        <Table striped bordered>
+                            <thead>
+                            <tr>
+                                <th>Couleur</th>
+                                <th>Quantité</th>
+                                <th>Sous total</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            {profilSaved.couleurs.map((item) => (
+                                <tr key={item.id}>
+                                    <td>
+                                        {item.nomCouleur}
+                                    </td>
+                                    <td>
+                                        {item.metreLineaire}
+                                    </td>
+                                    <td>
+                                        {item.metreLineaire * profil.prixUnitaire} €
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </Table>
+
+
+                    )}
+                    <Row></Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={handleCloseModalAjoutOk}>
+                        Ok
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 };
