328da98fcd2574f3f8f7a54f77f5a820468cc42f
maximelonguepe <longuepemaxime73@gmail.com>
2023-09-17 19:39:05 +0200

    Adding delete mapping

--- src/ListeObjets.tsx
+++ src/ListeObjets.tsx
@@ -1,23 +1,47 @@
 import React, {useState, useEffect, ChangeEvent} from "react";
-import {Table, Button, Row, Col, Form, FormControl} from "react-bootstrap";
+import {Table, Button, Row, Col, Form, FormControl, Modal} from "react-bootstrap";
 import Barcode from "react-barcode";
 import {Link} from "react-router-dom";
 import {apiUrl, Objet} from "./types";
+import {type} from "os";
 
 const ListeObjets = () => {
     const [objets, setObjets] = useState<Objet[]>([]);
     const [typeSelected, setTypeSelected] = useState('TOUS');
     const [idSearched, setIdSearched] = useState('');
+    const [showModalDeleteObjet, setShowModalDeleteObjet] = useState(false);
+    const [totalSum, setTotalSum] = useState(0);
+    const [objetSelected, setObjetSelected]=useState<Objet>({
+        type:"", id: 0, prixUnitaire: 0, quantiteOuMl: 0, referenceProduit: ""
 
+    })
     const fetchObjets = async () => {
         try {
             const response = await fetch(apiUrl + 'objets');
             const jsonData = await response.json();
+
             setObjets(jsonData);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
     };
+
+    const fetchdeleteObjet = async () => {
+        try {
+            // Remplacez 'URL_DE_VOTRE_API' par l'URL réelle de votre API
+            const response = await fetch(apiUrl + `objets?id=${objetSelected.id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json', // Vous pouvez ajuster les en-têtes selon vos besoins
+                },
+            }
+
+            );
+
+        }catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
 
 
     const fetchObjetsFilter = async () => {
@@ -28,6 +52,7 @@ const ListeObjets = () => {
         try {
             const response = await fetch(apiUrl + `objets/filter?type=${typeSelected}&id=${idX}`);
             const jsonData = await response.json();
+
             setObjets(jsonData);
             renderList();
         } catch (error) {
@@ -37,6 +62,7 @@ const ListeObjets = () => {
 
     useEffect(() => {
         fetchObjets();
+
     }, []);
     const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
         setTypeSelected(event.target.value);
@@ -44,6 +70,24 @@ const ListeObjets = () => {
 
     const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setIdSearched(event.target.value);
+    };
+
+    function closeModalDeleteObjet(){
+        setShowModalDeleteObjet(false);
+    }
+    function findObjectById(array:Array<Objet>, id:number) {
+        return array.find(obj => obj.id === id);
+    }
+    const onclickShowModalDeleteObjet = (id: number) => {
+        const objetFound = findObjectById(objets, id);
+
+        if (objetFound) {
+            setObjetSelected(objetFound);
+            setShowModalDeleteObjet(true);
+        } else {
+            // Gérer le cas où l'objet n'est pas trouvé, par exemple, afficher une erreur ou une notification.
+            console.error(`Aucun objet trouvé avec l'ID ${id}`);
+        }
     };
 
     const renderList = () => {
@@ -131,14 +175,58 @@ const ListeObjets = () => {
                                             <Button variant="primary">Modifier</Button>
                                         </Link>
                                     </td>
+                                    <td>
+                                        <Button variant="danger" onClick={()=>onclickShowModalDeleteObjet(item.id)}>Supprimer</Button>
+                                    </td>
                                 </tr>
+
                             ))}
+                            <tr>
+                                <td>
+                                    Total
+                                </td>
+                                <td/>
+                                <td/>
+                                <td/>
+                                <td/>
+                                <td>
+                                    {totalSum} €
+                                </td>
+                            </tr>
                             </tbody>
                         </Table>
                     </>
                 ) : (
                     <p>Pas d'objets à montrer pour le moment veuillez en créer</p>
                 )}
+                <Modal show={showModalDeleteObjet} onHide={closeModalDeleteObjet}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Etes vous sur de supprimer l'objet suivant</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Row>
+                            <Col>
+                                Id :
+                            </Col>
+                            <Col>
+                                {objetSelected.id}
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                Référence :
+                            </Col>
+                            <Col>
+                                {objetSelected.referenceProduit}
+                            </Col>
+                        </Row>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="danger" onClick={fetchdeleteObjet}>
+                            Supprimer
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </>
         );
     };
